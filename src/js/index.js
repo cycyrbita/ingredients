1f49e518d68b96573c2b372de6da87692e5bc6ef
Nikita job <cycyrbita@mail.ru>
2021-07-30 19:04:38 +0300

    домо

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,53 @@
 
 $(document).ready(function() {
-    // запускаем слайдер
     $('.owl-carousel').owlCarousel({
         items: 1,
+        nav: true,
         dots: false,
-        nav: true
-    });
+        navText: false
+    })
 
-    // поворот карточки
-    $('.ingredients__btn_rotate').click(function() {
-        $(this).parents('.ingredients__info').toggleClass('active');
-    });
+    readyCards();
 
-    // подсвечиваем метки с языками
-    $('.ingredients__metka span').click(function() {
-        $('.ingredients__metka span').not($(this).addClass('active')).removeClass('active');
-    });
-
-    // скролл для имени и для описания
-    $('.ingredients__back .ingredients__description, .ingredients__back .ingredients__name').mCustomScrollbar({
-        axis: 'y',
-        theme: 'rounded-dots-dark'
-    });
-
-    // скролл для метки с языкаи
-    $('.ingredients__metka').mCustomScrollbar({
-        axis: 'x',
-        theme: 'dark-thin',
-        advanced: { autoExpandHorizontalScroll: true }
-    });
-
-    // функция обрезания текста
-    function cutLongText(text, textSize, finalLength) {
-        $(text).each(function() {
-            if ($(this).text().length > textSize) {
-                $(this).html($(this).text().slice(0, finalLength) + '...');
-            }
-        });
+    // копирование текста
+    function copytext(elem) {
+        let $field = $('<textarea>');
+        $('body').append($field);
+        $field.val($(elem).text()).select();
+        document.execCommand('copy');
+        $field.remove();
     }
-    cutLongText('.ingredients__front .ingredients__description p', 90, 90);
-    cutLongText('.ingredients__front .ingredients__name', 36, 36);
 
-    // стилизуем инпуты
-    $('input, select').styler();
+    // функции карточки
+    function readyCards() {
+        $('.ingredients').on('click', '.js-card__edit',function() {
+            $(this).parents('.card').toggleClass('edit');
+        })
 
-    // открываем и скрываем сайдбар
-    $('.sidebar__open').click(function() {
-        $('.ingredients__sidebar').toggleClass('active');
-    });
+        $('.ingredients').on('click', '.card__description span, .card__name span', function() {
+            copytext($(this).parent().children('p'));
+        })
+
+        $('.ingredients').on('click', '.card__language-body span', function() {
+            $(this).parent('.card__language-body').find('span').removeClass('active');
+            $(this).addClass('active');
+        })
+
+        $('.ingredients').on('click', '.card__field-add div', function() {
+            let text = $(this).prev().val();
+            let allElem = $(this).parents('.card__tags').find('.card__tags-tags span');
+
+            if(text) {
+
+                for(let i = 0; i < allElem.length; i++) {
+                    if(allElem[i].innerText != text) {
+                        $(this).parents('.card__tags').find('.card__tags-tags').append(`<span>${$(this).prev().val()} <mark></mark></span>`);
+                        return
+                    } else {
+                        return
+                    }
+                }
+            }
+        })
+    }
 });
